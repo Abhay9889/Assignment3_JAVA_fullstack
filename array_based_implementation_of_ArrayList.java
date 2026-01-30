e32be6a52a024533f7d7e83acf70603fb7e08ca5
Abhay9889 <147751808+Abhay9889@users.noreply.github.com>
2026-01-31 01:07:47 +0530

    Implement MyArrayList with basic operations

---
+++ array_based_implementation_of_ArrayList.java
@@ -0,0 +1,89 @@
+public class MyArrayList<T> implements MyList<T> {
+
+    private Object[] data;
+    private int size;
+    private int capacity;
+
+    public MyArrayList() {
+        capacity = 1;
+        data = new Object[capacity];
+        size = 0;
+    }
+
+    private void resize() {
+        capacity = capacity * 2;
+        Object[] temp = new Object[capacity];
+
+        for (int i = 0; i < size; i++) {
+            temp[i] = data[i];
+        }
+
+        data = temp;
+    }
+
+    public void add(T element) {
+
+        if (size == capacity) {
+            resize();
+        }
+
+        data[size] = element;
+        size++;
+    }
+
+    public void insert(T element, int index) {
+
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index out of range");
+        }
+
+        if (size == capacity) {
+            resize();
+        }
+
+        for (int i = size; i > index; i--) {
+            data[i] = data[i - 1];
+        }
+
+        data[index] = element;
+        size++;
+    }
+
+    public T get(int index) {
+
+        if (isEmpty()) {
+            throw new IllegalStateException("List is empty");
+        }
+
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of range");
+        }
+
+        return (T) data[index];
+    }
+
+    public void delete(int index) {
+
+        if (isEmpty()) {
+            throw new IllegalStateException("List is empty");
+        }
+
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of range");
+        }
+
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+
+        size--;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
